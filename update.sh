e3effb6f8d1906e78088d6af7159efbd954eb3e0
Lukas Macko <lmacko@cisco.com>
2017-10-12 09:53:36 +0200

    Test01

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+config=$(<conf)
+clonePath=$(echo $config | cut -d ' ' -f 1)
+project=$(echo $config | cut -d ' ' -f 2)
+repo=$(echo $config | cut -d ' ' -f 3)
+commit=$(echo $config | cut -d ' ' -f 4)
+
+cd $GOPATH/src
+mkdir -p ${clonePath}
+cd ${clonePath}
+git clone ${repo}
+cd ${project}
+rm -rf vendor
+cp ~/glideUpdater/glide.yaml .
+glide install --strip-vendor
+
